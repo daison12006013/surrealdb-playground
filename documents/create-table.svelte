19763d9bc64ecaea14a8e1cbb3993c29075c0b35
Daison Carino <daison12006013@gmail.com>
2022-10-04 23:33:24 +0800

    Add create table

---
+++ documents/create-table.svelte
@@ -0,0 +1,11 @@
+<h1>Using CREATE</h1>
+<div>
+	<p>
+		The CREATE statement stores a record into a specific table and able to link a foreign record.
+	</p>
+	<p>
+		To <a target="_blank" href="https://surrealdb.com/docs/surrealql/statements/create"
+			>learn more</a
+		> about create
+	</p>
+</div>
